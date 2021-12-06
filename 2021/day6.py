05074005f213379a07ff4670eef039087be9f601
Kailey van Zomeren <kailey.van.zomeren@pwc.com>
2021-12-06 08:49:11 +0100

    Short version

--- 2021/day6.py
+++ 2021/day6.py
@@ -1,9 +1,6 @@
-
 from functions.generic import *
 from functions.load_data import load_data
-# from functions.load_data import load_data_split_cast_to_int as load_data
 from functions.test import test
-import numpy as np
 
 INPUT_DIR = f"./inputs/"
 FILE_NAME = os.path.basename(__file__).replace('.py', 'a.txt')
@@ -16,30 +13,11 @@ def prep_data(data):
     return fishes
 
 
-def simulation_old(data, days):
-
-    _data = data[0].split(',')
-    fishes = np.array(_data, dtype=int)
-    for i in range(days):
-        new_fish = fishes[fishes == 0]
-        fishes[fishes == 0] = 7
-        fishes -= 1
-        fishes = np.append(fishes, [8] * len(new_fish))
-    return fishes
-
-
 def simulation(fishes, days):
     for _ in range(days):
-        new_fishes = [0] * 9
-        for fish, count in enumerate(fishes):
-            if fish == 0:
-                born_fishes = re_fishes = count
-                print(born_fishes)
-            else:
-                new_fishes[fish - 1] = count
-        new_fishes[8] += born_fishes
-        new_fishes[6] += re_fishes
-        fishes = new_fishes
+        new_fishes = fishes.pop(0)
+        fishes.append(new_fishes)
+        fishes[6] += new_fishes
     return sum(fishes)
 
 
@@ -60,3 +38,16 @@ if __name__ == "__main__":
     data = load_data(file_path)
     print(f"Answer to part 1 is {part1(data)}")
     print(f"Answer to part 2 is {part2(data)}")
+
+
+# Memory issues
+# def simulation_old(data, days):
+#
+#     _data = data[0].split(',')
+#     fishes = np.array(_data, dtype=int)
+#     for i in range(days):
+#         new_fish = fishes[fishes == 0]
+#         fishes[fishes == 0] = 7
+#         fishes -= 1
+#         fishes = np.append(fishes, [8] * len(new_fish))
+#     return fishes
